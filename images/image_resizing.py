2ec5470d438a5f5418a48792b7450ce05e884eda
Sam Rodriguez <sarl132040@gmail.com>
2021-04-02 15:09:38 +0200

    rename piece images

--- images/image_resizing.py
+++ images/image_resizing.py
@@ -1,8 +1,8 @@
 from PIL import Image
 
 basewidth = 50
-img = Image.open('chess-piece.png')
+img = Image.open('white_pawn_original.png')
 wpercent = (basewidth/float(img.size[0]))
 hsize = int((float(img.size[1])*float(wpercent)))
 img = img.resize((basewidth,hsize), Image.ANTIALIAS)
-img.save('white_pawn.png')
+img.save('white_pawn_50px.png')
