10d757b1733864567e7a673fb76b2c7d50718113
Sam Rodriguez <sarl132040@gmail.com>
2021-04-02 15:28:31 +0200

    resize piece images

--- images/image_resizing.py
+++ images/image_resizing.py
@@ -1,8 +1,16 @@
 from PIL import Image
+import os
 
 basewidth = 50
-img = Image.open('white_pawn_original.png')
-wpercent = (basewidth/float(img.size[0]))
-hsize = int((float(img.size[1])*float(wpercent)))
-img = img.resize((basewidth,hsize), Image.ANTIALIAS)
-img.save('white_pawn_50px.png')
+
+path = r'C:\Users\Sam\Documents\Python Scripts\Chess\images'
+
+for file in os.listdir(path):
+    if 'original' in file:
+        tokens = file.split("_")
+        f_img = path+"/"+file
+        img = Image.open(f_img)
+        wpercent = (basewidth/float(img.size[0]))
+        hsize = int((float(img.size[1])*float(wpercent)))
+        img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+        img.save(tokens[0]+'_'+tokens[1]+'_'+str(basewidth)+'px.png')
